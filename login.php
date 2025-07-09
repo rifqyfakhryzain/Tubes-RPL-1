7ce76e24d29eca99dbaf0f74d4cc51690b5a6580
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-07-09 21:55:32 +0700

    proses login

--- login.php
+++ login.php
@@ -28,9 +28,9 @@
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form class="space-y-6" action="proses/proses_login.php" method="POST">
                 <div>
-                    <label for="email" class="block text-sm/6 font-medium text-gray-900">Email address</label>
+                    <label for="username" class="block text-sm/6 font-medium text-gray-900">Email address</label>
                     <div class="mt-2">
-                        <input type="email" name="email" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                        <input type="email" name="username" id="email" autocomplete="email" required class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                     </div>
                 </div>
 
@@ -47,7 +47,7 @@
                 </div>
 
                 <div>
-                    <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+                    <button type="submit" name="submit_validasi" value="abc" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
                 </div>
             </form>
 
--- proses/proses_login.php
+++ proses/proses_login.php
@@ -1,3 +1,16 @@
 <?php
-echo "login";
+$username = (isset($_POST['username'])) ?  htmlentities($_POST['username']) : "";
+$password = (isset($_POST['password'])) ?  htmlentities($_POST['password']) : "";
+if (!empty($_POST['submit_validasi'])) {
+    if ($username == "rifqyfz678@gmail.com" && $password == "rifqy") {
+        header('Location: ../index.php');
+    } else { ?>
+        <script>
+            alert('Username atau password yang anda masukkan salah')
+            window.location = '../login.php'
+        </script>
+<?php
+    }
+}
+
 ?>
