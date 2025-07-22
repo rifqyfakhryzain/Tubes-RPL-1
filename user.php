f12053d382a2b5d570dd3ac0c2fdf7406f552588
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-07-22 21:02:34 +0700

    Coba Tabel User

--- user.php
+++ user.php
@@ -1,25 +1,23 @@
 <?php
 session_start();
 if (empty($_SESSION['username_dapoer'])) {
-    header('Location: login.php');
-    exit();
+   header('Location: login.php');
+   exit();
 }
 
 if ($_SESSION['level_dapoer'] != 1) {
-    // Jika bukan level 1, redirect ke halaman utama
-    header('location: index.php');
-    exit();
+   // Jika bukan level 1, redirect ke halaman utama
+   header('location: index.php');
+   exit();
 }
 
-
 include 'proses/connect.php';
-
 
 $username = $_SESSION['username_dapoer'];
 $query = mysqli_query($conn, "SELECT * FROM tabel_user WHERE username = '$username'");
 
 if ($query) {
-    $hasil = mysqli_fetch_assoc($query);
+   $hasil = mysqli_fetch_assoc($query);
 }
 ?>
 
@@ -41,11 +39,11 @@ if ($query) {
 <body class="min-h-screen flex flex-col">
 
    <!-- Navbar -->
-    <?php include "header.php"; ?>
+   <?php include "header.php"; ?>
    <!-- End Navbar -->
 
    <!-- Sidebar -->
-    <?php include "sidebar.php" ;?>
+   <?php include "sidebar.php"; ?>
    <!-- EndSidebar -->
 
    <main class="flex-grow pt-16 p-4 sm:ml-64">
@@ -54,23 +52,128 @@ if ($query) {
          <div class="bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg shadow-sm p-6 md:p-10 space-y-6">
 
             <!-- Judul About -->
-            <h3 class="text-1xl font-bold text-grey-900 dark:text-white">Customer</h3>
+            <h3 class="text-1xl font-bold text-grey-900 dark:text-white">Halaman User</h3>
 
             <!-- Konten -->
-            <div class="space-y-4">
-               <h2 class="text-3xl font-extrabold tracking-tight text-gray-900 dark:text-white">
-                  Ini adalah halaman User
-               </h2>
-               <p class="text-gray-600 dark:text-gray-400">
-                  Empower Developers, IT Ops, and business teams to collaborate at high velocity. Respond to changes and deliver great customer and employee service experiences fast.
-               </p>
-               <a href="#" class="inline-flex items-center font-medium text-blue-600 hover:text-blue-800 dark:text-blue-500 dark:hover:text-blue-700">
-                  Learn more
-                  <svg class="w-2.5 h-2.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-                  </svg>
-               </a>
+            <div class="relative overflow-x-auto">
+               <div class="flex justify-end">
+                  <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Tambah User</button>
+               </div>
+
+               <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                  <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                     <tr>
+                        <th scope="col" class="px-6 py-3">
+                           No
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                           Nama
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                           Username
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                           Level
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                           No HP
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                           Aksi
+                        </th>
+                     </tr>
+                  </thead>
+                  <tbody>
+                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                           Apple MacBook Pro 17"
+                        </th>
+                        <td class="px-6 py-4">
+                           Silver
+                        </td>
+                        <td class="px-6 py-4">
+                           Laptop
+                        </td>
+                        <td class="px-6 py-4">
+                           $2999
+                        </td>
+                        <td class="px-6 py-4">
+                           $2999
+                        </td>
+                        <td class="px-6 py-4">
+                           <div class="flex gap-2">
+                              <!-- Button Lihat -->
+                              <button type="button" class="flex items-center justify-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                                 <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-width="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
+                                    <path stroke="currentColor" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                 </svg>
+                              </button>
+
+
+                              <!-- Button Edit -->
+                              <button type="button" class="flex items-center justify-center gap-2 text-white bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 focus:outline-none dark:focus:ring-yellow-800">
+                                 <svg class="w-4 h-4 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 5V4a1 1 0 0 0-1-1H8.914a1 1 0 0 0-.707.293L4.293 7.207A1 1 0 0 0 4 7.914V20a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-5M9 3v4a1 1 0 0 1-1 1H4m11.383.772 2.745 2.746m1.215-3.906a2.089 2.089 0 0 1 0 2.953l-6.65 6.646L9 17.95l.739-3.692 6.646-6.646a2.087 2.087 0 0 1 2.958 0Z" />
+                                 </svg>
+                              </button>
+
+                              <!-- Button Delete -->
+                              <button type="button" class="flex items-center justify-center gap-2 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">
+                                 <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                    viewBox="0 0 24 24">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                       d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+                                 </svg>
+                              </button>
+
+
+                              <!-- Button password -->
+                              <button type="button" class="flex items-center justify-center gap-2 text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-500 dark:hover:bg-gray-600 focus:outline-none dark:focus:ring-gray-800">
+                                 <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd" d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-3a1 1 0 0 1 1-1Z" clip-rule="evenodd" />
+                                 </svg>
+                              </button>
+
+                           </div>
+
+
+                        </td>
+
+
+                     </tr>
+                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                           Microsoft Surface Pro
+                        </th>
+                        <td class="px-6 py-4">
+                           White
+                        </td>
+                        <td class="px-6 py-4">
+                           Laptop PC
+                        </td>
+                        <td class="px-6 py-4">
+                           $1999
+                        </td>
+                     </tr>
+                     <tr class="bg-white dark:bg-gray-800">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                           Magic Mouse 2
+                        </th>
+                        <td class="px-6 py-4">
+                           Black
+                        </td>
+                        <td class="px-6 py-4">
+                           Accessories
+                        </td>
+                        <td class="px-6 py-4">
+                           $99
+                        </td>
+                     </tr>
+                  </tbody>
+               </table>
             </div>
+
 
          </div>
       </div>
