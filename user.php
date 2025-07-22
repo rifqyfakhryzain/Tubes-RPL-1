5475d470058d3046873d62fe0f0e8b44b1661aa8
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-07-23 06:02:31 +0700

    Membuat Form User

--- user.php
+++ user.php
@@ -14,12 +14,18 @@ if ($_SESSION['level_dapoer'] != 1) {
 include 'proses/connect.php';
 
 $username = $_SESSION['username_dapoer'];
+
+// Data untuk header.php
+$query_user = mysqli_query($conn, "SELECT * FROM tabel_user WHERE username = '$username'");
+$hasil = mysqli_fetch_assoc($query_user);
+
+// Data untuk tabel semua user
+$result = [];
 $query = mysqli_query($conn, "SELECT * FROM tabel_user");
-
-
 while ($record = mysqli_fetch_assoc($query)) {
    $result[] = $record;
 }
+
 
 ?>
 
@@ -188,7 +194,7 @@ while ($record = mysqli_fetch_assoc($query)) {
 
    </main>
 
-   <!-- Modal Tambah,Edit<Lihat USer -->
+   <!-- Modal Tambah,Edit,Lihat USer -->
    <!-- Main modal -->
    <div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
       <div class="relative p-4 w-full max-w-md max-h-full">
@@ -197,7 +203,7 @@ while ($record = mysqli_fetch_assoc($query)) {
             <!-- Modal header -->
             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                  Create New Product
+                  Tambah User
                </h3>
                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="crud-modal">
                   <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
@@ -206,30 +212,43 @@ while ($record = mysqli_fetch_assoc($query)) {
                   <span class="sr-only">Close modal</span>
                </button>
             </div>
-            <!-- Modal body -->
-            <form class="p-4 md:p-5">
+            <!-- Modal body dan Form -->
+            <form action="proses/proses.input.php" method="POST" class="p-4 md:p-5">
                <div class="grid gap-4 mb-4 grid-cols-2">
+                  <!-- Nama -->
                   <div class="col-span-2">
-                     <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                     <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="">
+                     <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
+                     <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Nama" required="">
+                  </div>
+                  <!-- Username -->
+                  <div class="col-span-2">
+                     <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                     <input type="email" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Username" required="">
+                  </div>
+                  <!-- Password -->
+                  <div class="col-span-2 sm:col-span-1">
+                     <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                     <input type="name" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Password" required="">
                   </div>
                   <div class="col-span-2 sm:col-span-1">
-                     <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Price</label>
-                     <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required="">
-                  </div>
-                  <div class="col-span-2 sm:col-span-1">
-                     <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Category</label>
+                     <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Level User</label>
                      <select id="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                        <option selected="">Select category</option>
-                        <option value="TV">TV/Monitors</option>
-                        <option value="PC">PC</option>
-                        <option value="GA">Gaming/Console</option>
-                        <option value="PH">Phones</option>
+                        <option selected="">Pilih Level User</option>
+                        <option value="owner">Owner/Admin</option>
+                        <option value="kasir">Kasir</option>
+                        <option value="pelayan">Pelayan</option>
+                        <option value="dapur">Dapur</option>
                      </select>
                   </div>
+                   <!-- No HP -->
                   <div class="col-span-2">
-                     <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Product Description</label>
-                     <textarea id="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write product description here"></textarea>
+                     <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NO HP</label>
+                     <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="N HP" required="">
+                  </div>
+                  <!-- Alamat -->
+                  <div class="col-span-2">
+                     <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Alamat</label>
+                     <textarea id="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Alamat description here"></textarea>
                   </div>
                </div>
                <button type="submit" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
