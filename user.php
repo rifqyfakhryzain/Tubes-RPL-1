4c09db89ecc6d118366f80cc970db2c95cc9a4ac
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-07-23 10:53:31 +0700

    nambah MODAL LIHAT USER

--- user.php
+++ user.php
@@ -131,7 +131,7 @@ while ($record = mysqli_fetch_assoc($query)) {
                               <td class="px-6 py-4">
                                  <div class="flex gap-2">
                                     <!-- Button Lihat -->
-                                    <button type="button" data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="flex items-center justify-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                                    <button type="button" data-modal-target="lihat-modal-<?php echo $row['id']; ?>" data-modal-toggle="lihat-modal-<?php echo $row['id']; ?>" class="flex items-center justify-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                                        <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                           <path stroke="currentColor" stroke-width="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
                                           <path stroke="currentColor" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
@@ -139,29 +139,30 @@ while ($record = mysqli_fetch_assoc($query)) {
                                     </button>
 
 
-                                    <!-- Button Edit -->
+
+                                    <!-- Button Edit
                                     <button type="button" data-modal-target="crud-modal" data-modal-toggle="crud-modal" class="flex items-center justify-center gap-2 text-white bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 focus:outline-none dark:focus:ring-yellow-800">
                                        <svg class="w-4 h-4 text-white dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 5V4a1 1 0 0 0-1-1H8.914a1 1 0 0 0-.707.293L4.293 7.207A1 1 0 0 0 4 7.914V20a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-5M9 3v4a1 1 0 0 1-1 1H4m11.383.772 2.745 2.746m1.215-3.906a2.089 2.089 0 0 1 0 2.953l-6.65 6.646L9 17.95l.739-3.692 6.646-6.646a2.087 2.087 0 0 1 2.958 0Z" />
                                        </svg>
-                                    </button>
+                                    </button> -->
 
-                                    <!-- Button Delete -->
+                                    <!-- Button Delete
                                     <button type="button" data-modal-target="popup-modal" data-modal-toggle="popup-modal" class="flex items-center justify-center gap-2 text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">
                                        <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                                           viewBox="0 0 24 24">
                                           <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                              d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
                                        </svg>
-                                    </button>
+                                    </button> -->
 
 
-                                    <!-- Button password -->
+                                    <!-- Button password
                                     <button type="button" class="flex items-center justify-center gap-2 text-white bg-gray-600 hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-gray-500 dark:hover:bg-gray-600 focus:outline-none dark:focus:ring-gray-800">
                                        <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
                                           <path fill-rule="evenodd" d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-3a1 1 0 0 1 1-1Z" clip-rule="evenodd" />
                                        </svg>
-                                    </button>
+                                    </button> -->
 
                                  </div>
 
@@ -194,7 +195,7 @@ while ($record = mysqli_fetch_assoc($query)) {
 
    </main>
 
-   <!-- Modal Tambah,Edit,Lihat USer -->
+   <!-- Modal Tambah USer -->
    <!-- Main modal -->
    <div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
       <div class="relative p-4 w-full max-w-md max-h-full">
@@ -213,7 +214,7 @@ while ($record = mysqli_fetch_assoc($query)) {
                </button>
             </div>
             <!-- Modal body dan Form -->
-           <form action="proses/proses.input.php" method="POST" class="p-4 md:p-5">
+            <form action="proses/proses.input.php" method="POST" class="p-4 md:p-5">
                <div class="grid gap-4 mb-4 grid-cols-2">
                   <!-- Nama -->
                   <div class="col-span-2">
@@ -231,17 +232,17 @@ while ($record = mysqli_fetch_assoc($query)) {
                      <input type="name" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Password" required="">
                   </div>
                   <!-- Level -->
-<div class="col-span-2 sm:col-span-1">
-  <label for="level" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Level User</label>
-  <select name="level" id="level" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-    <option value="" disabled selected hidden>Pilih Level User</option>
-    <option value="1">Owner/Admin</option>
-    <option value="2">Kasir</option>
-    <option value="3">Pelayan</option>
-    <option value="4">Dapur</option>
-  </select>
-</div>
-                   <!-- No HP -->
+                  <div class="col-span-2 sm:col-span-1">
+                     <label for="level" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Level User</label>
+                     <select name="level" id="level" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        <option value="" disabled selected hidden>Pilih Level User</option>
+                        <option value="1">Owner/Admin</option>
+                        <option value="2">Kasir</option>
+                        <option value="3">Pelayan</option>
+                        <option value="4">Dapur</option>
+                     </select>
+                  </div>
+                  <!-- No HP -->
                   <div class="col-span-2">
                      <label for="no_hp" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NO HP</label>
                      <input type="text" name="no_hp" id="no_hp" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="N HP" required="">
@@ -263,6 +264,79 @@ while ($record = mysqli_fetch_assoc($query)) {
          </div>
       </div>
    </div>
+
+   <?php
+   foreach ($result as $row) { ?>
+   <!-- Modal Lihat USer -->
+   <!-- Main modal -->
+   <div id="lihat-modal-<?php echo $row['id']; ?>" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+      <div class="relative p-4 w-full max-w-md max-h-full">
+         <!-- Modal content -->
+         <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+            <!-- Modal header -->
+            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200">
+               <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                  Lihat User
+               </h3>
+               <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="lihat-modal-<?php echo $row['id']; ?>">
+                  <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                  </svg>
+                  <span class="sr-only">Close modal</span>
+               </button>
+            </div>
+            <!-- Modal body dan Form -->
+            <form action="proses/proses.input.php" method="POST" class="p-4 md:p-5">
+               <div class="grid gap-4 mb-4 grid-cols-2">
+                  <!-- Nama -->
+                  <div class="col-span-2">
+                     <label for="nama" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nama</label>
+                     <input type="text" name="nama" id="nama" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Nama" required="" value="<?php echo $row['nama'] ?>">
+                  </div>
+                  <!-- Username -->
+                  <div class="col-span-2">
+                     <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                     <input type="email" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Username" required="" value="<?php echo $row['username'] ?>">
+                  </div>
+                  <!-- Password -->
+                  <div class="col-span-2 sm:col-span-1">
+                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                     <input type="name" name="password" id="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Password" required=""value="<?php echo $row['password'] ?>" >
+                  </div>
+                  <!-- Level -->
+                  <div class="col-span-2 sm:col-span-1">
+                     <label for="level" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Level User</label>
+                     <select name="level" id="level" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" value="<?php echo $row['level'] ?>">
+                        <option value="" disabled selected hidden>Pilih Level User</option>
+                        <option value="1">Owner/Admin</option>
+                        <option value="2">Kasir</option>
+                        <option value="3">Pelayan</option>
+                        <option value="4">Dapur</option>
+                     </select>
+                  </div>
+                  <!-- No HP -->
+                  <div class="col-span-2">
+                     <label for="no_hp" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">NO HP</label>
+                     <input type="text" name="no_hp" id="no_hp" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="N HP" required="" value="<?php echo $row['no_hp'] ?>">
+                  </div>
+                  <!-- Alamat -->
+                  <div class="col-span-2">
+                     <label for="alamat" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"> Alamat</label>
+                     <textarea name="alamat" id="alamat" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Alamat description here" > <?php echo $row['alamat']; ?></textarea>
+                  </div>
+               </div>
+               <!-- SUbmit -->
+               <button type="submit" name="input_user_validate" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                  <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                     <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
+                  </svg>
+                  Tambah User
+               </button>
+            </form>
+         </div>
+      </div>
+   </div>
+   <?php } ?>
 
    <!-- modal Delete -->
    <div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
