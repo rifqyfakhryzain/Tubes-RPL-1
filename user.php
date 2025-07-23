d4a65a8cf0b634c9b95aba57d1139a61921edf9f
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-07-23 11:04:27 +0700

    Hapus Button Tambah User

--- user.php
+++ user.php
@@ -330,13 +330,6 @@ while ($record = mysqli_fetch_assoc($query)) {
                      <textarea disabled name="alamat" id="alamat" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Alamat description here" > <?php echo $row['alamat']; ?></textarea>
                   </div>
                </div>
-               <!-- SUbmit -->
-               <button type="submit" name="input_user_validate" class="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                  <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                     <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
-                  </svg>
-                  Tambah User
-               </button>
             </form>
          </div>
       </div>
