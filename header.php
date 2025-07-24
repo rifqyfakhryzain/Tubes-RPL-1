140cc8a1111fdb90ffbe083820bf33306b42a8e1
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-07-24 10:21:59 +0700

    Menambah name di bagian dropdown

--- header.php
+++ header.php
@@ -26,7 +26,7 @@
                     <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-sm shadow-sm dark:bg-gray-700 dark:divide-gray-600" id="dropdown-user">
                         <div class="px-4 py-3" role="none">
                             <p class="text-sm text-gray-900 dark:text-white" role="none">
-                                Neil Sims
+                                <?php echo $hasil['nama'] ?>
                             </p>
                             <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
                                 <?php echo $hasil['username'] ?>
