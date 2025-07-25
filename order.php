29c69ccf8a0d457316b7afd3646e9b3dbf7bed51
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-07-25 19:49:08 +0700

    ganti Kategori

--- order.php
+++ order.php
@@ -247,7 +247,7 @@ while ($record = mysqli_fetch_assoc($query)) {
                   <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                   </svg>
-                  <span class="sr-only">Close </span>
+                  <span class="sr-only">Close modal</span>
                </button>
             </div>
             <!-- Modal body dan Form -->
@@ -274,7 +274,7 @@ while ($record = mysqli_fetch_assoc($query)) {
                      <select name="kategori" id="kategori" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                         <option value="" disabled selected hidden>Pilih Kategori Menu</option>
                         <option value="1">Makanan</option>
-                        <option value="2">Minuman</option>
+                        <option value="2">HALO</option>
                      </select>
                   </div>
                </div>
