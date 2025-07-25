d3b750520f5e792f3ede26df284c571fc9d783e2
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-07-25 19:48:37 +0700

    Ganti Close Modal

--- order.php
+++ order.php
@@ -152,6 +152,13 @@ while ($record = mysqli_fetch_assoc($query)) {
                               </td>
                               <!-- Status -->
                               <td class="px-6 py-4">
+                                
+                                 <?php if ($row['status'] == 1) {
+                                    echo 'Masuk Dapur';
+                                 } elseif ($row['status'] == 2) {
+                                    echo 'Makanan tersedia';
+                                 }
+                                 ?>
                               </td>
                               <!-- Waktu Order -->
                               <td class="px-6 py-4">
@@ -240,7 +247,7 @@ while ($record = mysqli_fetch_assoc($query)) {
                   <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                   </svg>
-                  <span class="sr-only">Close modal</span>
+                  <span class="sr-only">Close </span>
                </button>
             </div>
             <!-- Modal body dan Form -->
