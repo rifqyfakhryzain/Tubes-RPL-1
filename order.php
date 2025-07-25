67d5a6c8728f930367348d619e2028bb16ddde07
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-07-25 19:50:06 +0700

    Ganti Button MOdal jadi Tambah Order

--- order.php
+++ order.php
@@ -274,7 +274,7 @@ while ($record = mysqli_fetch_assoc($query)) {
                      <select name="kategori" id="kategori" required class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                         <option value="" disabled selected hidden>Pilih Kategori Menu</option>
                         <option value="1">Makanan</option>
-                        <option value="2">HALO</option>
+                        <option value="2">Minuman</option>
                      </select>
                   </div>
                </div>
@@ -283,7 +283,7 @@ while ($record = mysqli_fetch_assoc($query)) {
                   <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                      <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
                   </svg>
-                  Tambah User
+                  Tambah Order
                </button>
             </form>
          </div>
