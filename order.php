4ff6556d4c477030de0d1786cdf9c2d4fe7e4b66
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-07-26 12:14:59 +0700

    Mengganti Warna Aksi Lihat di order.php

--- order.php
+++ order.php
@@ -168,7 +168,7 @@ while ($record = mysqli_fetch_assoc($query)) {
                               <td class="px-6 py-4">
                                  <div class="flex gap-2">
                                     <!-- Button Lihat -->
-                                    <button type="button" data-modal-target="lihat-modal-<?php echo $row['id_order']; ?>" data-modal-toggle="lihat-modal-<?php echo $row['id_order']; ?>" class="flex items-center justify-center gap-2 text-white bg-yellow-500 hover:bg-yellow-600 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 focus:outline-none dark:focus:ring-yellow-800">
+                                    <button type="button" data-modal-target="lihat-modal-<?php echo $row['id_order']; ?>" data-modal-toggle="lihat-modal-<?php echo $row['id_order']; ?>" class="flex items-center justify-center gap-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                                        <svg class="w-4 h-4 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                           <path stroke="currentColor" stroke-width="2" d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
                                           <path stroke="currentColor" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
