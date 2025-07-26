0e4212ff4e810e8102ab4e109c0bd36be00ea510
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-07-27 06:22:30 +0700

    Menambah proses delete order

--- order.php
+++ order.php
@@ -361,17 +361,17 @@ while ($record = mysqli_fetch_assoc($query)) {
                   <span class="sr-only">Close modal</span>
                </button>
 
-               <form action="proses/proses_delete_menu.php" method="POST" class="p-4 md:p-5">
-                  <input type="hidden" name="id" value="<?php echo $row['id_order']; ?>">
+               <form action="proses/proses_delete_order.php" method="POST" class="p-4 md:p-5">
+                  <input type="hidden" name="id_order" value="<?php echo $row['id_order']; ?>">
                   <div class="p-4 md:p-5 text-center">
                      <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                      </svg>
                      <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this product?</h3>
-                     <button data-modal-hide="popup-modal-<?= $row['id']; ?>" name="delete_menu_validate" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                     <button data-modal-hide="popup-modal-<?= $row['id_order']; ?>" name="delete_order_validate" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
                         Iyakk
                      </button>
-                     <button data-modal-hide="popup-modal-<?= $row['id']; ?>" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
+                     <button data-modal-hide="popup-modal-<?= $row['id_order']; ?>" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
                   </div>
             </div>
          </div>
