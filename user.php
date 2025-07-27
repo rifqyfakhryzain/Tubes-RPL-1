785d847775d724deda56316527b5a4937025e8cb
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-07-27 10:06:21 +0700

    Menambah validasi di modal delete user

--- user.php
+++ user.php
@@ -395,7 +395,7 @@ while ($record = mysqli_fetch_assoc($query)) {
                            <?php
                            $data = array("Owner/Admin", "Kasir", "Pelayan", "Dapur");
                            foreach ($data as $key => $value) {
-                              $level_value = $key + 1; // karena level di DB dimulai dari 1
+                              $level_value = $key + 1;
                               $selected = ($row["level"] == $level_value) ? "selected" : "";
                               echo "<option value='$level_value' $selected>$value</option>";
                            }
@@ -452,13 +452,22 @@ while ($record = mysqli_fetch_assoc($query)) {
                      <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                      </svg>
-                     <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
-                        Apakah anda yakin menghapus user
-                        <span class="font-bold text-black dark:text-white"><?php echo $row['nama']; ?></span>
-                     </h3>
-                     <button data-modal-hide="popup-modal-<?= $row['id']; ?>" name="delete_user_validate" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
-                        Hapus
-                     </button>
+
+                     <!-- Validasi Tidak bisa Hapus Akun Sendiri -->
+                     <?php if ($row['username'] == $_SESSION['username_dapoer']): ?>
+                        <h3 class="mb-5 text-lg font-normal text-red-600 dark:text-red-400">
+                           Anda tidak bisa menghapus akun sendiri.
+                        </h3>
+                     <?php else: ?>
+                        <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                           Apakah anda yakin menghapus user
+                           <span class="font-bold text-black dark:text-white"><?php echo $row['nama']; ?></span>
+                        </h3>
+                        <button data-modal-hide="popup-modal-<?= $row['id']; ?>" name="delete_user_validate" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                           Hapus
+                        </button>
+                     <?php endif; ?>
+
                      <button data-modal-hide="popup-modal-<?= $row['id']; ?>" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Batal</button>
                   </div>
             </div>
