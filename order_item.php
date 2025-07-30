a256dfac2fdcfe22d8eb8bfb95d8a1fb9cf6d284
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-07-30 12:37:30 +0700

    Mengganti button Tambah Item

--- order_item.php
+++ order_item.php
@@ -318,7 +318,7 @@ if ($id_order > 0) {
                             <svg class="me-1 -ms-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clip-rule="evenodd"></path>
                             </svg>
-                            Tambah Order
+                            Tambah Item
                         </button>
                     </form>
                 </div>
