dc04d64ad61e3c9b190e4516d87bf4e1aea1c4fd
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-07-30 11:34:50 +0700

    Menambah Dokumentasi Validasi Jika tidak ada data menu

--- order_item.php
+++ order_item.php
@@ -121,16 +121,7 @@ if ($id_order > 0) {
                         </div>
                     </div>
 
-
-
-                    <div class="w-full flex justify-start sm:justify-end">
-                        <!-- Button Tambah User -->
-                        <button type="button" data-modal-target="crud-modal" data-modal-toggle="crud-modal"
-                            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 mb-5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 whitespace-nowrap">
-                            Tambah Order
-                        </button>
-                    </div>
-
+                    <!-- Validasi Jika data menu tidak ada -->
                     <?php
                     if (empty($result)) {
                         echo "Data Menu tidak ada";
