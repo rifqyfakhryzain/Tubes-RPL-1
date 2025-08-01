6aed54a744a2c42b530a7c5286efb87d1d2de712
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-08-01 07:16:43 +0700

    Menambah porses delete order item

---
+++ proses/proses_delete_order_item.php
@@ -0,0 +1,20 @@
+<?php
+include "connect.php";
+
+$id_order = isset($_POST['id_order']) ? htmlentities($_POST['id_order']) : "";
+
+if (isset($_POST['delete_order_validate'])) {
+    if ($id_order == "") {
+        echo "<script>alert('ID tidak ditemukan.'); window.history.back();</script>";
+        exit;
+    }
+
+    $query = mysqli_query($conn, "DELETE FROM tabel_order WHERE id_order = '$id_order'");
+
+    if ($query) {
+        echo "<script>alert('Order berhasil dihapus.'); window.location.href='../order.php';</script>";
+    } else {
+        echo "<script>alert('Gagal Menghapus Order " . mysqli_error($conn) . "'); window.history.back();</script>";
+    }
+}
+?>
