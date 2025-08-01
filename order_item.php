979df3269dad927857225b1ca93bdbd813fef67a
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-08-01 07:18:32 +0700

    Mengubah name dan id form dan button delete item

--- order_item.php
+++ order_item.php
@@ -413,7 +413,7 @@ if ($id_order > 0) {
                         <span class="sr-only">Close modal</span>
                     </button>
 
-                    <form action="proses/proses_delete_menu.php" method="POST" class="p-4 md:p-5">
+                    <form action="proses/proses_delete_order_item.php" method="POST" class="p-4 md:p-5">
                         <input type="hidden" name="id" value="<?php echo $row['id_list_order']; ?>">
                         <div class="p-4 md:p-5 text-center">
                             <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
@@ -428,7 +428,7 @@ if ($id_order > 0) {
                      <h3 class="mb-5 text-xl font-normal text-gray-500 dark:text-gray-400">  
                         <span class="font-bold text-black dark:text-white">Jumlah :  <?php echo $row['jumlah']; ?></span>
                      </h3>
-                     <button data-modal-hide="popup-modal-<?= $row['id_list_order']; ?>" name="delete_order_validate" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                     <button data-modal-hide="popup-modal-<?= $row['id_list_order']; ?>" name="delete_order_item_validate" type="submit" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
                         Hapus
                      </button>
                      <button data-modal-hide="popup-modal-<?= $row['id_list_order']; ?>" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Batal</button>
