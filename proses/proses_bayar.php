93193684ad69cd560a1cbef97e28348db7a715af
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-08-01 15:31:14 +0700

    Menambah proses bayar

--- proses/proses_bayar.php
+++ proses/proses_bayar.php
@@ -7,42 +7,42 @@ session_start();
 $kode_order = isset($_POST['kode_order']) ? htmlentities($_POST['kode_order']) : "";
 $meja = isset($_POST['meja']) ? htmlentities($_POST['meja']) : "";
 $pelanggan = isset($_POST['pelanggan']) ? htmlentities($_POST['pelanggan']) : ""; 
-$catatan = isset($_POST['catatan']) ? htmlentities($_POST['catatan']) : ""; 
-$menu = isset($_POST['menu']) ? htmlentities($_POST['menu']) : ""; 
-$jumlah = isset($_POST['jumlah']) ? htmlentities($_POST['jumlah']) : ""; 
+$total = isset($_POST['total']) ? htmlentities($_POST['total']) : ""; 
+$uang = isset($_POST['uang']) ? htmlentities($_POST['uang']) : ""; 
+$kembalian = $uang - $total;
 
-// Ambil ID user dari session (pelayan)
-$id_pelayan = $_SESSION['id']; // pastikan id sudah disimpan saat login
+// // Ambil ID user dari session (pelayan)
+// $id_pelayan = $_SESSION['id']; // pastikan id sudah disimpan saat login
 
 $message = "";
 
-if (isset($_POST['input_order_item_validate'])) {
-
-    // Cek apakah kode_order sudah dipakai
-    $cek = mysqli_query($conn, "SELECT * FROM tabel_list_order WHERE menu = '$menu' && kode_order='$kode_order'");
-    if (mysqli_num_rows($cek) > 0) {
-        $message = '<script>
-            alert("Gagal: Kode Order sudah digunakan.");
+if (isset($_POST['bayar_validate'])) {
+    if ($kembalian<0) {
+            $message = '<script>
+            alert("Nominal uang tidak mencukupi");
             window.history.back();
         </script>';
     } else {
         // INSERT ke database
-        $query = mysqli_query($conn, "INSERT INTO tabel_list_order (menu,kode_order,jumlah,catatan) 
-                                      VALUES ('$menu', '$kode_order', '$jumlah', '$catatan')");
+        $query = mysqli_query($conn, "INSERT INTO tabel_bayar (id_bayar,nominal_uang,total_bayar) 
+                                      VALUES ('$kode_order', '$uang', '$total')");
 
         if ($query) {
             $message = '<script>
-                alert("Data berhasil dimasukkan.");
+                alert("Pembayaran Berhasil \nUang Kembalian RP. '.$kembalian.'");
                 window.location.href = "../order_item.php?id_order=' . $kode_order . '";
             </script>';
         } else {
             $message = '<script>
-                alert("Terjadi kesalahan: ' . mysqli_error($conn) . '");
+                alert("Pembayaran Gagal ' . mysqli_error($conn) . '");
                 window.history.back();
             </script>';
         }
     }
-}
+
+    }
+
+
 
 echo $message;
 ?>
