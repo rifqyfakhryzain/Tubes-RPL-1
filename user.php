fe617ef0f34b2174b21bba52af4f56c4daeb8e20
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-08-08 16:18:44 +0700

    Mengubah tampilan user

--- user.php
+++ user.php
@@ -97,7 +97,7 @@ while ($record = mysqli_fetch_assoc($query)) {
                               No
                            </th>
                            <th scope="col" class="px-6 py-3">
-                              
+                              Nama
                            </th>
                            <th scope="col" class="px-6 py-3">
                               Username
