0cfaf0112ea3def6fae2432bbbfe03c512c31b95
Rifqy Fakhry Zain <rifqyfz678@gmail.com>
2025-08-22 06:32:19 +0700

    tambah css mobile

--- report.php
+++ report.php
@@ -57,6 +57,10 @@
       <!-- Simple-DataTables (untuk Flowbite DataTable) -->
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" />
       <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" defer></script>
+
+      <!-- Css Mobile -->
+      <link rel="stylesheet" href="asset/css/mobile.css">
+
    </head>
 
 
